0b42e98f6fc02a36363fb46c7cb0a2eb2da5a378
SirCabby <77202713+SirCabby@users.noreply.github.com>
2025-11-15 12:23:31 -0500

    fixed freeze time

--- CabbyCodes/cabbycodes-freeze-time.js
+++ CabbyCodes/cabbycodes-freeze-time.js
@@ -21,15 +21,59 @@
     }
 
     const settingKey = 'freezeTimeOfDay';
-    const timeVariableIds = [16]; // Variable 16 tracks the current in-game hour.
+    const defaultTimeVariableIds = [];
+    const trackedVariableIds = new Set(defaultTimeVariableIds);
+    const frozenValues = {};
+
+    const detectionConfig = {
+        minValue: 0,
+        maxValue: 2880,
+        increments: [1, 5, 6, 10, 12, 15, 20, 30, 45, 60],
+        requiredHits: 1
+    };
+
+    const detectionState = {
+        hits: Object.create(null)
+    };
+
+    const initializationWindowMs = 5000;
+    let freezeActivatedAt = 0;
+
+    function nowMs() {
+        if (typeof performance !== 'undefined' && typeof performance.now === 'function') {
+            return performance.now();
+        }
+        return Date.now();
+    }
+
+    function addTimeVariableId(varId) {
+        if (!Number.isFinite(varId) || varId <= 0) {
+            return;
+        }
+        trackedVariableIds.add(varId);
+    }
+
+    function captureFrozenValues() {
+        trackedVariableIds.forEach(varId => {
+            if (typeof $gameVariables !== 'undefined' && $gameVariables) {
+                frozenValues[varId] = getTrueOriginalValue($gameVariables, varId);
+            }
+        });
+    }
 
     CabbyCodes.registerSetting(settingKey, 'Freeze Time of Day', {
         defaultValue: false,
         order: 60,
         onChange: newValue => {
-            CabbyCodes.log(
-                `[CabbyCodes] Time of day ${newValue ? 'is now frozen' : 'can advance again'}.`
-            );
+            if (newValue) {
+                freezeActivatedAt = nowMs();
+                setTimeout(() => captureFrozenValues(), 0);
+            } else {
+                freezeActivatedAt = 0;
+                trackedVariableIds.forEach(varId => {
+                    delete frozenValues[varId];
+                });
+            }
         }
     });
 
@@ -41,10 +85,9 @@
         if (!Number.isFinite(numericId)) {
             return false;
         }
-        return timeVariableIds.includes(numericId);
+        return trackedVariableIds.has(numericId);
     }
 
-    // Use CabbyCodes.callOriginal if available, otherwise fall back to manual lookup
     const callOriginal = (typeof CabbyCodes.callOriginal === 'function')
         ? CabbyCodes.callOriginal
         : (target, functionName, context, args) => {
@@ -55,22 +98,181 @@
             return undefined;
         };
 
+    const getTrueOriginalValue = (function() {
+        const originalValue = Game_Variables.prototype.value;
+        return function(instance, variableId) {
+            return originalValue.call(instance, variableId);
+        };
+    })();
+
+    function detectTimeVariableCandidate(varId, previousValue, newValue) {
+        if (trackedVariableIds.has(varId)) {
+            return null;
+        }
+        const numericPrev = Number(previousValue);
+        const numericNew = Number(newValue);
+        if (!Number.isFinite(numericPrev) || !Number.isFinite(numericNew)) {
+            return null;
+        }
+        if (
+            numericNew < detectionConfig.minValue ||
+            numericNew > detectionConfig.maxValue ||
+            numericPrev < detectionConfig.minValue ||
+            numericPrev > detectionConfig.maxValue
+        ) {
+            return null;
+        }
+        if (
+            freezeActivatedAt > 0 &&
+            nowMs() - freezeActivatedAt < initializationWindowMs &&
+            numericPrev === 0 &&
+            numericNew > 0
+        ) {
+            return null;
+        }
+        const diff = Math.abs(numericNew - numericPrev);
+        if (diff === 0) {
+            return null;
+        }
+        const matchesIncrement = detectionConfig.increments.some(inc => diff % inc === 0);
+        if (!matchesIncrement) {
+            return null;
+        }
+        detectionState.hits[varId] = (detectionState.hits[varId] || 0) + 1;
+        if (detectionState.hits[varId] >= detectionConfig.requiredHits) {
+            detectionState.hits[varId] = 0;
+            return {
+                newlyDetected: true,
+                captureValue: numericPrev
+            };
+        }
+        return null;
+    }
+
+    CabbyCodes.override(
+        Game_Variables.prototype,
+        'value',
+        function(variableId) {
+            const numericId = Number(variableId);
+            if (
+                shouldBlock(variableId) &&
+                Object.prototype.hasOwnProperty.call(frozenValues, numericId)
+            ) {
+                return frozenValues[numericId];
+            }
+            return callOriginal(Game_Variables.prototype, 'value', this, [variableId]);
+        }
+    );
+
     CabbyCodes.override(
         Game_Variables.prototype,
         'setValue',
         function(variableId, value) {
-            if (shouldBlock(variableId)) {
-                return this.value(Number(variableId));
+            const numericId = Number(variableId);
+            const previousValue = Number.isFinite(numericId)
+                ? getTrueOriginalValue(this, numericId)
+                : undefined;
+
+            if (Number.isFinite(numericId) && CabbyCodes.getSetting(settingKey, false)) {
+                const detectionResult = detectTimeVariableCandidate(numericId, previousValue, value);
+                if (detectionResult?.newlyDetected) {
+                    addTimeVariableId(numericId);
+                    if (!Object.prototype.hasOwnProperty.call(frozenValues, numericId)) {
+                        frozenValues[numericId] = detectionResult.captureValue;
+                    }
+                    return;
+                }
             }
+
+            const shouldPrevent = (() => {
+                if (!CabbyCodes.getSetting(settingKey, false)) {
+                    return false;
+                }
+                if (!Number.isFinite(numericId)) {
+                    return false;
+                }
+                return trackedVariableIds.has(numericId);
+            })();
+
+            if (shouldPrevent) {
+                if (!Object.prototype.hasOwnProperty.call(frozenValues, numericId)) {
+                    const currentValue = Number.isFinite(previousValue)
+                        ? previousValue
+                        : getTrueOriginalValue(this, numericId);
+                    frozenValues[numericId] = currentValue;
+                }
+                return;
+            }
+
             return callOriginal(Game_Variables.prototype, 'setValue', this, [
                 variableId,
                 value
             ]);
-        },
-        settingKey
+        }
     );
 
-    CabbyCodes.log('[CabbyCodes] Freeze Time module loaded');
+    function wrapDataWithProxy(instance) {
+        if (!instance._data || instance._cabbycodesDataProxy) {
+            return;
+        }
+        const originalData = instance._data;
+        instance._data = new Proxy(originalData, {
+            set(target, property, value) {
+                const propNum = Number(property);
+                if (
+                    Number.isFinite(propNum) &&
+                    CabbyCodes.getSetting(settingKey, false) &&
+                    !trackedVariableIds.has(propNum)
+                ) {
+                    const detectionResult = detectTimeVariableCandidate(propNum, target[property], value);
+                    if (detectionResult?.newlyDetected) {
+                        addTimeVariableId(propNum);
+                        if (!Object.prototype.hasOwnProperty.call(frozenValues, propNum)) {
+                            frozenValues[propNum] = detectionResult.captureValue;
+                        }
+                        return true;
+                    }
+                }
+                if (Number.isFinite(propNum) && shouldBlock(propNum)) {
+                    if (!Object.prototype.hasOwnProperty.call(frozenValues, propNum)) {
+                        const currentValue = target[property];
+                        frozenValues[propNum] =
+                            currentValue !== undefined && currentValue !== null ? currentValue : 0;
+                    }
+                    return true;
+                }
+                return Reflect.set(target, property, value);
+            },
+            get(target, property) {
+                const propNum = Number(property);
+                if (
+                    Number.isFinite(propNum) &&
+                    shouldBlock(propNum) &&
+                    Object.prototype.hasOwnProperty.call(frozenValues, propNum)
+                ) {
+                    return frozenValues[propNum];
+                }
+                return Reflect.get(target, property);
+            }
+        });
+        instance._cabbycodesDataProxy = true;
+    }
+
+    const originalInitialize = Game_Variables.prototype.initialize;
+    Game_Variables.prototype.initialize = function() {
+        originalInitialize.call(this);
+        wrapDataWithProxy(this);
+    };
+
+    const originalClear = Game_Variables.prototype.clear;
+    Game_Variables.prototype.clear = function() {
+        originalClear.call(this);
+        wrapDataWithProxy(this);
+    };
+
+    if (typeof $gameVariables !== 'undefined' && $gameVariables) {
+        wrapDataWithProxy($gameVariables);
+    }
 })();
 
 
